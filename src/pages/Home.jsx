70e1d570e2fa2fb1eadde92a3c29a1e041d3a34e
AlexDyatlov <alexdyatlov.web@gmail.com>
2021-01-30 01:09:24 +0300

    added preloader and redux-thunk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { Menu, PizzaBlock } from '../components';
+import { Menu, PizzaBlock, Preloader } from '../components';
 import { setCategory } from '../redux/actions/filters';
+import { fetchPizzas } from '../redux/actions/pizzas';
 
 function Home() {
   const dispatch = useDispatch();
   const items = useSelector(({pizzas}) => pizzas.items);
+  const isLoaded = useSelector(({pizzas}) => pizzas.isLoaded);
 
   const onSelectCategory = React.useCallback((index) => {
     dispatch(setCategory(index));
   }, []);
+
+  React.useEffect(() => {
+    if (!items.length) {
+      dispatch(fetchPizzas());
+    }
+  }, []); 
 
   return (
     <div className="container">
       <Menu onSelect={onSelectCategory} />
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items && items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj} />
-        ))}
+        {isLoaded 
+          ? items.map((obj) => <PizzaBlock key={obj.id} isLoading={true} {...obj} />)
+          : Array(10).fill(<Preloader />)}
       </div>
     </div>
   );
--- src/redux/reducers/pizzas.js
+++ src/redux/reducers/pizzas.js
@@ -10,6 +10,7 @@ const pizzasReducer = (state = initialState, action) => {
     return {
       ...state,
       items: action.payload,
+      isLoaded: true,
     }
   }
   return state;
