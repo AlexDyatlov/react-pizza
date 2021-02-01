2b466772314a2a1b1bb20d00006c105bf6cf00c3
AlexDyatlov <alexdyatlov.web@gmail.com>
2021-02-01 15:39:29 +0300

    added the final price, quantity of pizzas and new jsx page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Categories, SortPopup, PizzaBlock, Preloader } from '../components';
 import { setCategory, setSortBy } from '../redux/actions/filters';
 import { fetchPizzas } from '../redux/actions/pizzas';
+import { addPizzaToCart } from '../redux/actions/cart';
 
 const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 const sortItems = [ 
@@ -15,6 +16,7 @@ const sortItems = [
 function Home() {
   const dispatch = useDispatch();
   const items = useSelector(({pizzas}) => pizzas.items);
+  const cartItems = useSelector(({ cart }) => cart.items);
   const isLoaded = useSelector(({pizzas}) => pizzas.isLoaded);
   const { category, sortBy } = useSelector(({filters}) => filters);
 
@@ -24,7 +26,11 @@ function Home() {
 
   const onSelectSortType = React.useCallback((type) => {
     dispatch(setSortBy(type));
-  }, [])
+  }, []);
+
+  const handleAddPizzaToCart = (pizzaObj) => {
+    dispatch(addPizzaToCart(pizzaObj));
+  };
 
   React.useEffect(() => {
     dispatch(fetchPizzas(sortBy, category));
@@ -42,7 +48,12 @@ function Home() {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoaded 
-          ? items.map((obj) => <PizzaBlock key={obj.id} isLoading={true} {...obj} />)
+          ? items.map((obj) => 
+            <PizzaBlock 
+              onClickAddPizza={handleAddPizzaToCart} 
+              key={obj.id} {...obj}
+              addedCount={cartItems[obj.id] && cartItems[obj.id].length}
+            />)
           : Array(10).fill(0).map((_, index) => <Preloader key={index} />)}
       </div>
     </div>
